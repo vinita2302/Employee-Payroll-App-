48a215a30710c67cb85f11d00f0b9f3f5a63e73d
vinita patanker <vinitapatanker@gmail.com>
2025-02-27 13:17:36 +0530

    [vinita patanker] ADD :  UC2_Rest_controller

--- Employee-Payroll-App/src/main/java/com/spring/Employee_Payroll_App/controller/EmployeePayrollServiceController.java
+++ Employee-Payroll-App/src/main/java/com/spring/Employee_Payroll_App/controller/EmployeePayrollServiceController.java
@@ -1,45 +1,35 @@
 package com.spring.Employee_Payroll_App.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.spring.Employee_Payroll_App.model.EmployeePayroll;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollServiceController {
-
-    @Autowired
-    private EmployeePayrollService employeePayrollService;
 
     @GetMapping(value = {"", "/"})
     public String getEmployeePayrollByDefault(){
         return "Get Employee payroll default route";
     }
 
-    @GetMapping("/get/{name}")
-    public EmployeePayroll getEmployeePayrollByName(@PathVariable String name){
-        return employeePayrollService.getEmployeePayrollByName(name);
-    }
-
-    @GetMapping("/get/all")
-    public List<EmployeePayroll> getEmployeePayrolls(){
-        return employeePayrollService.getEmployeePayrolls();
+    @GetMapping("/get/{id}")
+    public String getEmployeePayrollById(@PathVariable String id){
+        return "Get Employee payroll of Id: "+id;
     }
 
     @PostMapping("/create")
-    public EmployeePayroll createEmployeePayroll(@RequestBody EmployeePayrollDto employeePayroll){
-        return employeePayrollService.createEmployeePayroll(employeePayroll);
+    public String createEmployeePayroll(@RequestBody EmployeePayroll employeePayroll){
+        return "Create Employee payroll with name: "+employeePayroll.getName()+" and salary as "+employeePayroll.getSalary();
     }
 
     @PutMapping("/update")
-    public EmployeePayroll updateEmployeePayroll(@RequestBody EmployeePayrollDto employeePayroll){
-        return employeePayrollService.updateEmployeePayroll(employeePayroll);
+    public String updateEmployeePayroll(@RequestBody EmployeePayroll employeePayroll){
+        return "Update Employee payroll for name: "+employeePayroll.getName()+" and salary as "+employeePayroll.getSalary();
     }
 
     @DeleteMapping("/delete/{id}")
     public String deleteEmployeePayrollById(@PathVariable String id){
-        return employeePayrollService.deleteEmployeePayrollById(Long.parseLong(id));
+        return "Delete Employee payroll of Id: "+id;
     }
 
 }
